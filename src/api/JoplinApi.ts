91a1f9222ae64a5a73a41d5adcafa727e1842761
rxliuli <rxliuli@gmail.com>
2020-07-24 20:43:13 +0800

    实现扫描本地 Joplin 端口的功能

--- src/api/JoplinApi.ts
+++ src/api/JoplinApi.ts
@@ -1,13 +1,38 @@
 import axios from 'axios'
-import { ApiUtil } from '../util/ApiUtil'
+import { config } from '..'
 
 class JoplinApi {
-  async scan(): Promise<number> {
-    throw new Error('no impl')
-  }
-  async ping(): Promise<boolean> {
-    const res = await axios.get<string>(ApiUtil.baseUrl('/ping'))
+  private static async pingPort(port: number) {
+    const res = await axios.get<string>(`http://localhost:${port}/ping`)
     return res.data === 'JoplinClipperServer'
+  }
+
+  private static range(begin: number, end: number) {
+    const res = []
+    for (let i = begin; i < end; i++) {
+      res.push(i)
+    }
+    return res
+  }
+
+  async scan(): Promise<number> {
+    const list = await Promise.all(
+      JoplinApi.range(41184, 41194 + 1).filter(async port => {
+        try {
+          return await JoplinApi.pingPort(port)
+        } catch (e) {
+          return false
+        }
+      }),
+    )
+    if (list.length === 0) {
+      throw new Error(`Joplin's port is not scanned`)
+    }
+    return list[0]
+  }
+
+  async ping(): Promise<boolean> {
+    return JoplinApi.pingPort(config.port)
   }
 }
 
