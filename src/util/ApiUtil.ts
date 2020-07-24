fe2a61730f481990ec3ca0ae612f3f6f6f3d84e9
rxliuli <rxliuli@gmail.com>
2020-07-24 21:05:53 +0800

    Api 工具类支持使用数组类型的 query 参数（comma 风格）

--- src/util/ApiUtil.ts
+++ src/util/ApiUtil.ts
@@ -3,10 +3,15 @@ import { config } from './config'
 
 export class ApiUtil {
   static baseUrl(url: string, param?: object) {
-    const query = stringify({
-      ...param,
-      token: config.token,
-    })
+    const query = stringify(
+      {
+        ...param,
+        token: config.token,
+      },
+      {
+        arrayFormat: 'comma',
+      },
+    )
     return `http://localhost:${config.port}${url}?${query}`
   }
 }
