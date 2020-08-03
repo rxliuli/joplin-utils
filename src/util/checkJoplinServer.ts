5776c149d070e6330fce845d63908435554e61e4
RenatoXSR <renatoxsr@gmail.com>
2020-08-03 11:43:19 -0300

    FIX: log to console descriptive error messages
    Changed file:
    ./src/util/checkJoplinServer.ts

--- src/util/checkJoplinServer.ts
+++ src/util/checkJoplinServer.ts
@@ -8,15 +8,17 @@ import { appConfig } from '../config/AppConfig'
 export async function checkJoplinServer() {
   const errMsg = () =>
     vscode.window.showErrorMessage(
-      `Joplin's token/port is set incorrectly, unable to access Joplin service!`,
+      `Joplin's token/port is set incorrectly, unable to access Joplin service! Are you sure it is running and active?`,
     )
   try {
     if (!(await joplinApi.ping())) {
       errMsg()
+      console.log("Could not ping Joplin service.")
       return false
     }
   } catch (e) {
     errMsg()
+    console.log('Error message: \n',JSON.stringify(e))
     return false
   }
   if (!appConfig.token) {
