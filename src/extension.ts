9b4066e041379c641b6c562ff630d2546069a17d
rxliuli <rxliuli@gmail.com>
2020-09-05 21:25:13 +0800

    删除 markdown 编辑器增强的代码

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,6 @@ import * as nls from 'vscode-nls'
 import { checkJoplinServer } from './util/checkJoplinServer'
 import * as MarkdownIt from 'markdown-it'
 import { useJoplinLink } from './util/useJoplinLink'
-import { JoplinMarkdownProvider } from './model/JoplinMarkdownProvider'
 
 initDevEnv()
 
@@ -90,15 +89,6 @@ export async function activate(context: vscode.ExtensionContext) {
   //endregion
 
   //region register markdown support
-
-  vscode.languages.registerDefinitionProvider(
-    {
-      scheme: 'file',
-      language: 'markdown',
-      pattern: 'edit-*.md',
-    },
-    new JoplinMarkdownProvider(),
-  )
 
   return {
     extendMarkdownIt(md: MarkdownIt) {
