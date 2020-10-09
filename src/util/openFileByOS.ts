2b6540f3aac3ab6f0784d4ff28063d83bbdea57c
rxliuli <rxliuli@gmail.com>
2020-10-09 19:34:19 +0800

    实现在 vscode 中打开 markdown 预览中的 joplin 链接时自动托管给系统

---
+++ src/util/openFileByOS.ts
@@ -0,0 +1,13 @@
+import * as open from 'open'
+import { existsSync } from 'fs-extra'
+
+/**
+ * 使用系统打开一些资源
+ * @param filePath
+ */
+export function openResourceByOS(filePath: string) {
+  if (!existsSync(filePath)) {
+    return
+  }
+  open(filePath)
+}
