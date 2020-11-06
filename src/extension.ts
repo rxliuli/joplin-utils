75028cc107041ff63d7cee6fbebce68a2f567086
rxliuli <rxliuli@gmail.com>
2020-11-06 13:26:15 +0800

    将编辑器相关的操作抽离到单独的 EditorProvider 文件中

--- src/extension.ts
+++ src/extension.ts
@@ -16,6 +16,7 @@ import { getReferenceAtPosition, matchAll } from './util/utils'
 import { JoplinLinkRegex, JoplinResourceRegex } from './util/constant'
 import { formatSize } from './util/formatSize'
 import { isInFencedCodeBlock, isInCodeSpan } from './util/externalUtils'
+import { MDDocumentLinkProvider, MDHoverProvider } from './model/EditorProvider'
 
 initDevEnv()
 
@@ -131,86 +132,11 @@ export async function activate(context: vscode.ExtensionContext) {
     pattern: '**/edit-*.md',
   }
   context.subscriptions.push(
-    vscode.languages.registerDocumentLinkProvider(docFilter, {
-      async provideDocumentLinks(document: vscode.TextDocument) {
-        const results: vscode.DocumentLink[] = []
-
-        document
-          .getText()
-          .split(/\r?\n/g)
-          .forEach((lineText, lineNum) => {
-            // noinspection RegExpRedundantEscape
-            for (const match of matchAll(/(\[.*\]\()(.+)\)/g, lineText)) {
-              const markdownTokenLink = match[2]
-              if (markdownTokenLink) {
-                const offset = (match.index || 0) + match[1].length
-
-                if (
-                  isInFencedCodeBlock(document, lineNum) ||
-                  isInCodeSpan(document, lineNum, offset)
-                ) {
-                  continue
-                }
-
-                const linkStart = new vscode.Position(lineNum, offset)
-                const linkEnd = new vscode.Position(
-                  lineNum,
-                  offset + markdownTokenLink.length,
-                )
-
-                let link: string
-                if (JoplinLinkRegex.test(markdownTokenLink)) {
-                  const id = JoplinLinkRegex.exec(markdownTokenLink)![1]
-                  link = wrapLink(id, TypeEnum.Note)
-                  console.log('是笔记链接: ', link)
-                } else if (JoplinResourceRegex.test(markdownTokenLink)) {
-                  const id = JoplinResourceRegex.exec(markdownTokenLink)![1]
-                  link = wrapLink(id, TypeEnum.Resource)
-                  console.log('是资源: ', link)
-                } else {
-                  link = markdownTokenLink
-                  console.log('是普通链接：', link)
-                }
-
-                const documentLink = new vscode.DocumentLink(
-                  new vscode.Range(linkStart, linkEnd),
-                  vscode.Uri.parse(link),
-                )
-
-                documentLink.tooltip = 'Follow link'
-
-                results.push(documentLink)
-              }
-            }
-          })
-
-        return results
-      },
-    }),
-    vscode.languages.registerHoverProvider(docFilter, {
-      async provideHover(document, position) {
-        const markdownTokenLink = getReferenceAtPosition(document, position)
-        if (!markdownTokenLink) {
-          return
-        }
-        let content: string[]
-        if (JoplinLinkRegex.test(markdownTokenLink)) {
-          const id = JoplinLinkRegex.exec(markdownTokenLink)![1]
-          // link = wrapLink(id, TypeEnum.Note)
-          const note = await noteApi.get(id)
-          const title = note.title
-          content = [title.startsWith('#') ? title.substr(1).trimLeft() : title]
-        } else if (JoplinResourceRegex.test(markdownTokenLink)) {
-          const id = JoplinResourceRegex.exec(markdownTokenLink)![1]
-          const resource = await resourceApi.get(id)
-          content = [resource.title, formatSize(resource.size)]
-        } else {
-          content = [markdownTokenLink]
-        }
-        console.log('provideHover: ', content)
-        return new vscode.Hover(content)
-      },
-    }),
+    vscode.languages.registerDocumentLinkProvider(
+      docFilter,
+      new MDDocumentLinkProvider(),
+    ),
+    vscode.languages.registerHoverProvider(docFilter, new MDHoverProvider()),
   )
 
   //endregion
