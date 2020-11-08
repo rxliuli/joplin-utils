5ffd248b58809413ff1b7c333b9f210312fe28e6
rxliuli <rxliuli@gmail.com>
2020-11-08 17:37:40 +0800

    简化 rollup 配置，仅打包 esm/umd 版本

--- build/rollup.config.js
+++
@@ -1,5 +0,0 @@
-import es from './rollup.config.es'
-import dev from './rollup.config.dev'
-import prod from './rollup.config.prod'
-
-export default [es, dev, prod]
--- src/api/JoplinApi.ts
+++ src/api/JoplinApi.ts
@@ -1,44 +1,47 @@
-import { config } from '..'
-import { request } from '../util/ajax'
+// noinspection ES6PreferShortImport
+import { config } from "../util/config";
+import { request } from "../util/ajax";
 
 class JoplinApi {
   private static async pingPort(port: number) {
     const res = await request<string>({
       url: `http://localhost:${port}/ping`,
-      method: 'get',
-      responseType: 'text',
-    })
-    return res === 'JoplinClipperServer'
+      method: "get",
+      responseType: "text"
+    });
+    return res === "JoplinClipperServer";
   }
 
   private static range(begin: number, end: number) {
-    const res = []
+    const res = [];
     for (let i = begin; i < end; i++) {
-      res.push(i)
+      res.push(i);
     }
-    return res
+    return res;
   }
 
   async scan(): Promise<number> {
     const list = await Promise.all(
       JoplinApi.range(41184, 41194 + 1).filter(async port => {
         try {
-          return await JoplinApi.pingPort(port)
+          return await JoplinApi.pingPort(port);
         } catch (e) {
-          return false
+          return false;
         }
-      }),
-    )
+      })
+    );
     if (list.length === 0) {
-      throw new Error(`Joplin's port is not scanned`)
+      throw new Error(`Joplin's port is not scanned`);
     }
-    return list[0]
+    return list[0];
   }
 
   async ping(): Promise<boolean> {
-    console.log('config port: ', config.port)
-    return JoplinApi.pingPort(config.port)
+    console.log("config port: ", config.port);
+    return JoplinApi.pingPort(config.port);
   }
 }
 
-export const joplinApi = new JoplinApi()
+
+
+export const joplinApi = new JoplinApi();
