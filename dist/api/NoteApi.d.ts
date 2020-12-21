b58f32832c19870034c2fcb3bd4788fd2e3947eb
rxliuli <rxliuli@gmail.com>
2020-12-21 22:23:18 +0800

    打包 dist

--- dist/api/NoteApi.d.ts
+++ dist/api/NoteApi.d.ts
@@ -7,14 +7,15 @@ import { ResourceGetRes } from '../modal/ResourceGetRes';
 import { PageParam, PageRes } from '../modal/PageData';
 import { FieldsParam } from '../modal/FieldsParam';
 import { RequiredField } from '../types/RequiredFiled';
+import { CommonType } from '../modal/CommonType';
 /**
  * TODO 可以考虑使用 fields() 方法设置然后产生一个新的 Api 实例
  */
 declare class NoteApi {
     list(): Promise<PageRes<NoteGetRes>>;
     list<K extends keyof NoteProperties = keyof NoteGetRes>(pageParam: PageParam<NoteProperties> & FieldsParam<K>): Promise<PageRes<Pick<NoteProperties, K>>>;
-    get(id: string): Promise<NoteGetRes>;
-    get<K extends keyof NoteProperties = keyof NoteGetRes>(id: string, fields?: K[]): Promise<Pick<NoteProperties, K>>;
+    get(id: string): Promise<NoteGetRes & CommonType>;
+    get<K extends keyof NoteProperties = keyof NoteGetRes>(id: string, fields?: K[]): Promise<Pick<NoteProperties, K> & CommonType>;
     create(param: RequiredField<Partial<NoteProperties>, 'title'>): Promise<NoteCreateRes>;
     update(param: RequiredField<Partial<NoteProperties>, 'id'>): Promise<NoteUpdateRes>;
     remove(id: string): Promise<unknown>;
