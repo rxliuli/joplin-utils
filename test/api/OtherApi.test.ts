53032fec9d0c2fcfd0f6602ca52bde5bb156af6e
rxliuli <rxliuli@gmail.com>
2020-12-25 15:05:14 +0800

    添加一个测试用例 [测试根据 id 获取资源是否会获取到图像]

--- test/api/OtherApi.test.ts
+++ test/api/OtherApi.test.ts
@@ -1,9 +1,14 @@
-import { folderApi, noteApi, tagApi } from '../../src'
+import { config, noteApi, TypeEnum } from '../../src'
 
-describe('test', () => {
-  it.skip('clear', async () => {
-    const clear = async (api: any) =>
-      (await api.list()).map(({ id }: any) => api.remove(id))
-    await Promise.all([folderApi, noteApi, tagApi].flatMap(clear))
+describe('其它测试', () => {
+  beforeAll(() => {
+    config.token = process.env.token!
+    config.port = 41184
+  })
+  it('测试根据 id 获取资源是否会获取到图像', async () => {
+    const res = await noteApi.resourcesById('5d8273edf5a14756ad629358b6c8c005')
+    res.forEach((item) => {
+      expect(item.type_).toBe(TypeEnum.Resource)
+    })
   })
 })
