51a10ce88b11e17224d6a69ef253914dc705ca02
rxliuli <rxliuli@gmail.com>
2021-01-01 10:21:02 +0800

    在安装了相应扩展的情况下，使用 vscode.open 命令打开 .drawio/.km 后缀的文件

--- src/util/OpenFileService.ts
+++ src/util/OpenFileService.ts
@@ -2,92 +2,25 @@ import { existsSync } from 'fs-extra'
 import * as open from 'open'
 import * as vscode from 'vscode'
 import { Uri } from 'vscode'
-
-// noinspection JSUnusedLocalSymbols
-const extList = [
-  'plaintext',
-  'Log',
-  'log',
-  'scminput',
-  'bat',
-  'clojure',
-  'coffeescript',
-  'jsonc',
-  'c',
-  'cpp',
-  'csharp',
-  'css',
-  'dockerfile',
-  'ignore',
-  'fsharp',
-  'git-commit',
-  'git-rebase',
-  'diff',
-  'go',
-  'groovy',
-  'handlebars',
-  'hlsl',
-  'html',
-  'ini',
-  'properties',
-  'java',
-  'javascriptreact',
-  'javascript',
-  'jsx-tags',
-  'json',
-  'less',
-  'lua',
-  'makefile',
-  'markdown',
-  'objective-c',
-  'objective-cpp',
-  'perl',
-  'perl6',
-  'php',
-  'powershell',
-  'jade',
-  'python',
-  'r',
-  'razor',
-  'ruby',
-  'rust',
-  'scss',
-  'search-result',
-  'shaderlab',
-  'shellscript',
-  'sql',
-  'swift',
-  'typescript',
-  'typescriptreact',
-  'vb',
-  'xml',
-  'xsl',
-  'yaml',
-  'graphql',
-  'vue',
-  'drawio',
-  'svg',
-  'km',
-  'xmind',
-  'wallaby-output',
-  'wallaby-timeline',
-]
+import * as path from 'path'
 
 export class OpenFileService {
   async openByVSCode(filePath: string) {
+    const fileName = path.basename(filePath)
+    if (
+      (fileName.endsWith('drawio') &&
+        vscode.extensions.getExtension('hediet.vscode-drawio')) ||
+      (fileName.endsWith('km') &&
+        vscode.extensions.getExtension('eighthundreds.vscode-mindmap'))
+    ) {
+      await vscode.commands.executeCommand('vscode.open', Uri.file(filePath))
+      return
+    }
+
     if (!existsSync(filePath)) {
       return
     }
     vscode.env.openExternal(Uri.file(filePath))
-    // const fileName = path.basename(filePath)
-    // const languages = await vscode.languages.getLanguages()
-    // if (languages.includes('drawio')) {
-    //   languages.push('drawio.svg')
-    // }
-    // if (languages.some((ext) => fileName.endsWith(`.${ext}`))) {
-    //
-    // }
-    // console.log('path.extname(filePath): ', extname, languages)
   }
 
   async openFileByOS(filePath: string) {
