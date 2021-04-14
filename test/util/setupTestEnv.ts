a78729e3a6ab3aef723871311506de8a8d22f80a
rxliuli <rxliuli@gmail.com>
2021-04-14 18:42:41 +0800

    build: 修改 jest 测试启动器，将 joplin token 从环境变量读取

--- test/util/setupTestEnv.ts
+++ test/util/setupTestEnv.ts
@@ -2,6 +2,5 @@ import { config } from '../../src'
 
 export function setupTestEnv() {
   config.port = 27583
-  config.token =
-    '83b1ba8f63663441ed4cd513d2a2797e46e1348dac70e630001eea1989be10510d0ffe11530dbffbccf4bb23b97d2035b3f830939a0f8ac905b26b51ffbf62e3'
+  config.token = process.env.TOKEN!
 }
