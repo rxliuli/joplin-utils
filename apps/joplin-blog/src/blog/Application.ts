31344e1acfe6f4ede77097dbb9524ac30a4a2971
rxliuli <rxliuli@gmail.com>
2021-04-18 00:40:54 +0800

    feat(joplin-blog): 美化 joplin-blog cli，使用 ora 实现更好的交互式输出信息

--- apps/joplin-blog/src/blog/Application.ts
+++ apps/joplin-blog/src/blog/Application.ts
@@ -3,6 +3,7 @@ import { CommonNote, CommonResource, CommonTag } from '../model/CommonNote'
 import path from 'path'
 import { config, noteApi, PageUtil, searchApi, TypeEnum } from 'joplin-api'
 import { JoplinMarkdownUtil } from '../util/JoplinMarkdownUtil'
+import { uniqueBy } from '@liuli-util/array'
 
 export interface BaseIntegrated {
   /**
@@ -39,13 +40,19 @@ export interface ApplicationConfig {
   tag: string
 }
 
+type ProcessHook = (options: {
+  rate: number
+  all: number
+  title: string
+}) => void
+
 /**
  * 入口程序
  */
 export class Application {
   constructor(
-    private readonly config: ApplicationConfig,
-    private handler: BaseIntegrated,
+    readonly config: ApplicationConfig,
+    readonly handler: BaseIntegrated,
   ) {}
 
   async filter() {
@@ -75,13 +82,14 @@ export class Application {
     )
   }
 
-  async check() {
+  async check(): Promise<Error | true> {
     try {
       config.token = this.config.token
       config.port = this.config.port || 41184
       await noteApi.list({ fields: ['id'], limit: 1 })
+      return true
     } catch (e) {
-      throw e
+      return e
     }
   }
 
@@ -90,14 +98,108 @@ export class Application {
     //获取所有笔记及其相关的资源和标签
     const arr = await this.filter()
     console.log(`一共有 ${arr.length} 个笔记需要处理`)
-    const noteList = await new AsyncArray(
+    //读取笔记附件与标签
+    const noteList = await this.readNoteAttachmentsAndTags(arr, (options) => {
+      console.log(
+        `${options.rate}/${options.all} 正在读取笔记附件与标签: `,
+        options.title,
+      )
+    })
+    //初始化
+    await this.handler.init()
+    //解析并写入笔记
+    const replaceContentNoteList = await this.parseAndWriteNotes(
+      noteList,
+      (options) => {
+        console.log(
+          `${options.rate}/${options.all} 正在解析笔记中的 Joplin 内部链接与附件资源: ${options.title}`,
+          options.title,
+        )
+      },
+    )
+    await this.writeNote(replaceContentNoteList, (options) => {
+      console.log(
+        `${options.rate}/${options.all} 正在写入笔记: ${options.title}`,
+        options.title,
+      )
+    })
+    //复制资源
+    await this.copyResources(noteList, (options) => {
+      console.log(
+        `${options.rate}/${options.all} 正在处理资源: ${options.title}`,
+      )
+    })
+  }
+
+  async copyResources(
+    noteList: (CommonNote & {
+      resources: CommonResource[]
+      tags: CommonTag[]
+    })[],
+    process: ProcessHook,
+  ) {
+    let i = 0
+    const resourceList = uniqueBy(
+      noteList.flatMap((item) => item.resources),
+      (resource) => resource.id,
+    )
+    await AsyncArray.forEach(
+      resourceList,
+      asyncLimiting(async (resource: CommonResource) => {
+        i++
+        process({ rate: i, all: resourceList.length, title: resource.title })
+        const fileName = resource.id + '.' + resource.file_extension
+        await this.handler.copy(
+          path.resolve(this.config.joplinProfilePath, 'resources', fileName),
+        )
+      }, 10),
+    )
+  }
+
+  parseAndWriteNotes(
+    noteList: (CommonNote & {
+      resources: CommonResource[]
+      tags: CommonTag[]
+    })[],
+    process: ProcessHook,
+  ) {
+    return noteList.map((note, i) => {
+      process({ rate: i, all: noteList.length, title: note.title })
+      return {
+        ...note,
+        text: this.handler.parse(note),
+      }
+    })
+  }
+
+  async writeNote(
+    noteList: (CommonNote & { text: string })[],
+    process: (options: { rate: number; all: number; title: string }) => void,
+  ) {
+    let i = 0
+    await AsyncArray.forEach(noteList, async (item) => {
+      i++
+      process({ rate: i, all: noteList.length, title: item.title })
+      await this.handler.write(item)
+      return item
+    })
+  }
+
+  async readNoteAttachmentsAndTags(arr: CommonNote[], process: ProcessHook) {
+    let i = 0
+    return new AsyncArray(
       arr.map((note) => ({
         ...note,
         title: JoplinMarkdownUtil.trimTitle(note.title),
       })),
     ).map(
       asyncLimiting(async (note) => {
-        console.log('正在读取笔记附件与标签: ', note.title)
+        i++
+        process({
+          rate: i,
+          all: arr.length,
+          title: note.title,
+        })
         const [resources, tags] = await Promise.all([
           noteApi.resourcesById(note.id, ['id', 'title', 'file_extension']),
           noteApi.tagsById(note.id),
@@ -107,34 +209,6 @@ export class Application {
           resources: resources as CommonResource[],
           tags: tags as CommonTag[],
         }
-      }, 10),
-    )
-    //初始化
-    await this.handler.init()
-    //解析并写入笔记
-    await AsyncArray.forEach(
-      noteList.map((note) => {
-        console.log('正在解析笔记中的 Joplin 内部链接与附件资源: ', note.title)
-        return {
-          ...note,
-          text: this.handler.parse(note),
-        }
-      }),
-      async (item) => {
-        console.log('正在写入笔记: ', item.title)
-        await this.handler.write(item)
-        return item
-      },
-    )
-    //复制资源
-    await AsyncArray.forEach(
-      noteList.flatMap((item) => item.resources),
-      asyncLimiting(async (resource: CommonResource) => {
-        console.log('正在处理资源: ', path.basename(resource.title))
-        const fileName = resource.id + '.' + resource.file_extension
-        await this.handler.copy(
-          path.resolve(this.config.joplinProfilePath, 'resources', fileName),
-        )
       }, 10),
     )
   }
--- apps/joplin-blog/src/cli/BlogCommand.ts
+++ apps/joplin-blog/src/cli/BlogCommand.ts
@@ -1,5 +1,9 @@
 import { pathExists, readJson } from 'fs-extra'
-import { Application, ApplicationConfig } from '../blog/Application'
+import {
+  Application,
+  ApplicationConfig,
+  BaseIntegrated,
+} from '../blog/Application'
 import { HexoIntegrated, HexoIntegratedConfig } from '../blog/HexoIntegrated'
 import {
   VuepressIntegrated,
@@ -7,39 +11,110 @@ import {
 } from '../blog/VuepressIntegrated'
 import { i18n } from '../util/I18n'
 import path from 'path'
+import ora from 'ora'
+
+type JoplinBlogConfig = ApplicationConfig & {
+  type: 'hexo' | 'vuepress'
+} & (HexoIntegratedConfig | {})
 
 class BlogCommand {
-  private static async getJoplinIntegrated(configPath: string) {
-    const config = (await readJson(configPath)) as ApplicationConfig & {
-      type: 'hexo' | 'vuepress'
-    } & (HexoIntegratedConfig | {})
+  private static async getBlogApplication(config: JoplinBlogConfig) {
+    let integrated: BaseIntegrated
     switch (config.type) {
       case 'hexo':
-        await new Application(
-          config,
-          new HexoIntegrated(config as HexoIntegratedConfig),
-        ).gen()
+        integrated = new HexoIntegrated(config as HexoIntegratedConfig)
         break
       case 'vuepress':
-        await new Application(
-          config,
-          new VuepressIntegrated(config as VuepressIntegratedConfig),
-        ).gen()
+        integrated = new VuepressIntegrated(config as VuepressIntegratedConfig)
         break
+      default:
+        throw new Error('不支持的博客类型')
     }
+    return new Application(config, integrated)
   }
 
-  async gen() {
-    await i18n.load(await i18n.getLanguage())
+  async checkConfig(): Promise<JoplinBlogConfig | false> {
     const configPath = path.resolve('.joplin-blog.json')
-    // console.log('configPath: ', configPath)
     if (!(await pathExists(configPath))) {
       console.error(
         i18n.t("blog.Can't find configuration file _joplin-blog_json"),
       )
+      return false
+    }
+    return (await readJson(configPath)) as JoplinBlogConfig
+  }
+
+  async gen() {
+    await i18n.load(await i18n.getLanguage())
+    const config = await this.checkConfig()
+    if (!config) {
       return
     }
-    await BlogCommand.getJoplinIntegrated(configPath)
+    const application = await BlogCommand.getBlogApplication(config)
+    await this.run(application)
+  }
+
+  async run(app: Application) {
+    const checkInfo = await app.check()
+    if (checkInfo !== true) {
+      console.error('测试 joplin 服务失败: ', checkInfo)
+      return
+    }
+    const spinner = ora({
+      color: 'blue',
+    })
+
+    spinner.start('开始过滤 joplin 笔记')
+    const arr = await app.filter()
+    if (arr.length === 0) {
+      spinner.warn('没有需要处理的笔记').stopAndPersist()
+      return
+    }
+    spinner.stopAndPersist({
+      text: i18n.t(`blog.Filter to get {{length}} notes to be processed`, {
+        length: arr.length,
+      }),
+    })
+
+    spinner.start('开始读取笔记附件与标签')
+    const noteList = await app.readNoteAttachmentsAndTags(arr, (options) => {
+      spinner.text = `[${options.rate}/${options.all}] 正在读取笔记附件与标签: ${options.title}`
+    })
+    spinner.text = '结束读取笔记附件与标签'
+    spinner.stopAndPersist()
+
+    spinner.start('开始框架初始化动作')
+    await app.handler.init()
+    spinner.stopAndPersist({
+      text: '结束框架初始化动作',
+    })
+
+    spinner.start('开始解析笔记中的 Joplin 内部链接与附件资源')
+    const replaceContentNoteList = app.parseAndWriteNotes(
+      noteList,
+      (options) => {
+        spinner.text = `[${options.rate}/${options.all}] 正在解析笔记中的 Joplin 内部链接与附件资源: ${options.title}`
+      },
+    )
+    spinner.stopAndPersist({
+      text: '结束解析笔记中的 Joplin 内部链接与附件资源',
+    })
+
+    spinner.start('开始写入笔记到本地文件')
+    await app.writeNote(replaceContentNoteList, (options) => {
+      spinner.text = `${options.rate}/${options.all} 正在写入笔记到本地文件: ${options.title}`
+    })
+    spinner.stopAndPersist({ text: '结束写入笔记到本地文件' })
+
+    spinner.start('开始复制附件资源')
+    await app.copyResources(noteList, (options) => {
+      spinner.text = `${options.rate}/${options.all} 正在复制附件资源: ${options.title}`
+    })
+    spinner.stopAndPersist({ text: '结束复制附件资源' })
+
+    spinner.start().stopAndPersist({
+      text: '结束生成博客',
+    })
   }
 }
 
