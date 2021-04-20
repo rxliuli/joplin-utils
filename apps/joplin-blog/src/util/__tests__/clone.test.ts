f77c21bedf897a3501f953f6ef36404943dcf2f8
rxliuli <rxliuli@gmail.com>
2021-04-20 15:39:25 +0800

    docs(joplin-blog): 测试使用 git clone 项目

---
+++ apps/joplin-blog/src/util/__tests__/clone.test.ts
@@ -0,0 +1,18 @@
+﻿import git from 'isomorphic-git'
+import http from 'isomorphic-git/http/node'
+import path from 'path'
+import * as fs from 'fs'
+import { remove } from 'fs-extra'
+
+describe('测试使用 git 克隆项目', () => {
+  it('基本示例', async () => {
+    const dir = path.resolve(__dirname, 'temp/hexo-starter')
+    await remove(dir)
+    await git.clone({
+      url: 'https://github.com/rxliuli/hexo-starter',
+      dir: dir,
+      fs,
+      http,
+    })
+  })
+})
