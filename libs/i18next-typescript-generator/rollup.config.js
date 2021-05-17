c57d7099d4f0440361a386d5728a1fe010e3c6fd
rxliuli <rxliuli@gmail.com>
2021-05-17 19:30:10 +0800

    build: 删除 i18next-typescript-generator 额外的 tsconfig.build.bin.json 配置

---
+++ libs/i18next-typescript-generator/rollup.config.js
@@ -0,0 +1,47 @@
+﻿import typescript from 'rollup-plugin-typescript2'
+import json from '@rollup/plugin-json'
+
+/** @type {import('rollup').RollupOptions} */
+export default [
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: './dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: './dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript(),
+      // terser(), // minifies generated bundles
+    ],
+  },
+  {
+    input: './src/bin.ts',
+    output: [
+      {
+        file: './dist/bin.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfigOverride: {
+          compilerOptions: {
+            declaration: false,
+            declarationMap: false,
+          },
+        },
+      }),
+      json(),
+      // terser(), // minifies generated bundles
+    ],
+  },
+]
