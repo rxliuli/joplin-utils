ffb0db1e4f212f67e0e798c288ec9f73c4e3658f
rxliuli <rxliuli@gmail.com>
2021-06-21 04:35:36 +0800

    feat(i18next-typescript-generator): 为这个 cli 实现国际化

--- libs/i18next-typescript-generator/src/bin.ts
+++ libs/i18next-typescript-generator/src/bin.ts
@@ -1,13 +1,15 @@
 import { Command } from 'commander'
 import { GeneratorCommandProgram } from './service/GeneratorCommand'
+import { i18n } from './util/I18n'
 ;(async () => {
+  await i18n.load(await i18n.getLanguage())
   await new Command()
-    .option('-i, --input <input>', '包含一或多个翻译文件的目录')
-    .option('-w, --watch', '是否使用监视模式')
-    .description('根据 json 生成 .d.ts 类型定义')
-    .action(async (options: { input: string; watch: boolean }) => {
+    .option('-i, --input <input...>', i18n.t('cli.option.input'))
+    .option('-w, --watch', i18n.t('cli.option.watch'))
+    .description(i18n.t('cli.description'))
+    .action(async (options: { input: string[]; watch: boolean }) => {
       await new GeneratorCommandProgram().main({
-        dirs: [options.input],
+        dirs: options.input,
         watch: options.watch,
       })
     })
--- libs/i18next-typescript-generator/src/service/GeneratorCommand.ts
+++ libs/i18next-typescript-generator/src/service/GeneratorCommand.ts
@@ -6,6 +6,8 @@ import path from 'path'
 import { AsyncArray } from '@liuli-util/async'
 import { Watcher } from './Watcher'
 import ora from 'ora'
+import { i18n } from '../util/I18n'
+import { DateTime } from 'luxon'
 
 export class GeneratorCommandProgram {
   /**
@@ -23,9 +25,15 @@ export class GeneratorCommandProgram {
 
   // noinspection JSMethodCanBeStatic
   private async exec(dirPath: string) {
+    const formatter = 'yyyy-MM-dd hh:mm:ss'
     const spinner = ora({ color: 'blue' })
     const dtsPath = path.join(dirPath, 'index.d.ts')
-    spinner.start(`开始生成类型定义: ${dtsPath}`)
+    spinner.start(
+      i18n.t('generator.begin', {
+        time: DateTime.now().toFormat(formatter),
+        dtsPath,
+      }),
+    )
     const scanner = new Scanner()
     const parser = new Parser()
     const generator = new Generator()
@@ -33,6 +41,11 @@ export class GeneratorCommandProgram {
     const configs = parser.parse(locales)
     const code = generator.generate(configs)
     await writeFile(dtsPath, code)
-    spinner.stopAndPersist({ text: `结束生成类型定义: ${dtsPath}` })
+    spinner.stopAndPersist({
+      text: i18n.t('generator.end', {
+        time: DateTime.now().toFormat(formatter),
+        dtsPath,
+      }),
+    })
   }
 }
