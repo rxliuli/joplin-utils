a84249761460ab5321d0a9061d66586be6141978
rxliuli <rxliuli@gmail.com>
2021-06-21 20:24:25 +0800

    chore(joplin-batch-web): 将首页与 README 合并为一个

--- apps/joplin-batch-web/src/pages/home/HomePage.tsx
+++ apps/joplin-batch-web/src/pages/home/HomePage.tsx
@@ -1,26 +1,24 @@
 import * as React from 'react'
 import { Card } from 'antd'
 import MarkdownIt from 'markdown-it'
+import { useAsync } from 'react-use'
 
 type HomePageProps = {}
-
-const content = `# joplin-batch-web
-
-处理一些 Joplin 本身不支持的批量操作，以可视化界面的形式展现出来。
-
-## FAQ
-
-### 为什么不使用 cli 的形式
-
-cli 的形式不太适合这种场景，例如需要预览异常的附件或笔记，在命令行下比较麻烦。
-`
 
 const markdownIt = new MarkdownIt()
 
 export const HomePage: React.FC<HomePageProps> = () => {
+  const state = useAsync(async () => {
+    const resp = await fetch('./README.md')
+    return (await resp.text()) as string
+  })
   return (
-    <Card>
-      <div dangerouslySetInnerHTML={{ __html: markdownIt.render(content) }} />
+    <Card loading={state.loading}>
+      {state.value && (
+        <div
+          dangerouslySetInnerHTML={{ __html: markdownIt.render(state.value) }}
+        />
+      )}
     </Card>
   )
 }
