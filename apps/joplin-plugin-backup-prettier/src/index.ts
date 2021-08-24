15abc3f3dd58162b62ffa837792c1e1c3d551eae
rxliuli <rxliuli@gmail.com>
2021-08-24 22:25:00 +0800

    feat: 在 joplin-plugin-backup-prettier 中使用 joplin-plugin-api 包

--- apps/joplin-plugin-backup-prettier/src/index.ts
+++ apps/joplin-plugin-backup-prettier/src/index.ts
@@ -1,5 +1,8 @@
-import joplin from '@thejohnfreeman/joplin-plugin'
-import { FileSystemItem } from '@thejohnfreeman/joplin-plugin/types'
+import joplin, {
+  ExportModule,
+  FileSystemItem,
+  ImportModule,
+} from 'joplin-plugin-api'
 import { copy, mkdirp, writeFile } from 'fs-extra'
 import * as path from 'path'
 
@@ -11,50 +14,49 @@ function resourceDir(context: any) {
   return context.destPath + '/resources'
 }
 
+class DirExporter implements ExportModule {
+  description = 'Zip Export Directory'
+  format = 'zip'
+  target = FileSystemItem.Directory
+  isNoteArchive = true
+
+  async onInit(context: any) {
+    await mkdirp(destDir(context))
+    await mkdirp(resourceDir(context))
+  }
+
+  async onProcessItem(context: any, _itemType: number, item: any) {
+    const filePath = destDir(context) + '/' + item.id + '.json'
+    const serialized = JSON.stringify(item)
+    await writeFile(filePath, serialized, 'utf8')
+  }
+
+  async onProcessResource(context: any, _resource: any, filePath: string) {
+    const destPath = resourceDir(context) + '/' + path.basename(filePath)
+    await copy(filePath, destPath)
+  }
+
+  async onClose(_context: any) {}
+}
+
+class DirImporter implements ImportModule {
+  description = 'Zip Import Directory'
+  format = 'zip'
+  sources = [FileSystemItem.Directory]
+  isNoteArchive = true
+
+  async onExec(context: any) {
+    // In this case importing is a lot more complicated due to the need to avoid
+    // duplicate IDs, to validate data and ensure note links and
+    // resources are still working properly.
+    // See InteropService_Importer_Raw for an example.
+    console.info('Not implemented! Importing from:', context)
+  }
+}
+
 joplin.plugins.register({
   onStart: async function () {
-    await joplin.interop.registerExportModule({
-      description: 'Zip Export Directory',
-      format: 'zip',
-      target: FileSystemItem.Directory,
-      isNoteArchive: true,
-
-      onInit: async (context: any) => {
-        await mkdirp(destDir(context))
-        await mkdirp(resourceDir(context))
-      },
-
-      onProcessItem: async (context: any, _itemType: number, item: any) => {
-        const filePath = destDir(context) + '/' + item.id + '.json'
-        const serialized = JSON.stringify(item)
-        await writeFile(filePath, serialized, 'utf8')
-      },
-
-      onProcessResource: async (
-        context: any,
-        _resource: any,
-        filePath: string,
-      ) => {
-        const destPath = resourceDir(context) + '/' + path.basename(filePath)
-        await copy(filePath, destPath)
-      },
-
-      onClose: async (_context: any) => {},
-    })
-
-    await joplin.interop.registerImportModule({
-      description: 'Zip Import Directory',
-      format: 'zip',
-      sources: [FileSystemItem.Directory],
-      isNoteArchive: true,
-
-      onExec: async (context: any) => {
-        // In this case importing is a lot more complicated due to the need to avoid
-        // duplicate IDs, to validate data and ensure note links and
-        // resources are still working properly.
-        // See InteropService_Importer_Raw for an example.
-        console.info('Not implemented! Importing from:', context)
-      },
-    })
+    await joplin.interop.registerExportModule(new DirExporter())
+    await joplin.interop.registerImportModule(new DirImporter())
   },
 })
