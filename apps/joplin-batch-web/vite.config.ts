25696bc64e68a286edb2a0dbeec4f5bbb82325e1
rxliuli <rxliuli@gmail.com>
2021-08-30 09:56:46 +0800

    chore: 使用 vite 插件优化 antd 引入的内容大小

--- apps/joplin-batch-web/vite.config.ts
+++ apps/joplin-batch-web/vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          //参考：http://article.docway.net/details?id=606bf9250a6c642cafe256e6
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      },
+    },
+  },
 })
