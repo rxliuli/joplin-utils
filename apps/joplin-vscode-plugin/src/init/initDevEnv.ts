a812b10bdf490e6f8466af523b3f60e7d29d0886
rxliuli <rxliuli@gmail.com>
2021-11-08 11:03:42 +0800

    chore: 删除项目中单独的 eslint 配置文件，修复 vscode 环境变量没有使用 NODE_ENV 的问题

--- apps/joplin-vscode-plugin/src/init/initDevEnv.ts
+++ apps/joplin-vscode-plugin/src/init/initDevEnv.ts
@@ -8,7 +8,7 @@ export function initDevEnv() {
     process.env.JOPLIN_TOKEN,
     process.env.JOPLIN_PORT,
   )
-  if (!process.env.DEBUG) {
+  if (process.env.NODE_ENV !== 'development') {
     return
   }
   // rxliuli: do not insert your own vars to the code. Use something else for that
