d322006b28a8e8316d8eae89c77654aaa966b7bc
rxliuli <rxliuli@gmail.com>
2022-01-04 00:54:35 +0800

    refactor: 使用 react-markdown 替换原始的 markdown-it

--- apps/joplin-batch-web/src/views/home/HomeView.tsx
+++ apps/joplin-batch-web/src/views/home/HomeView.tsx
@@ -1,27 +1,20 @@
 import * as React from 'react'
 import { Card } from 'antd'
-import MarkdownIt from 'markdown-it'
 import README from '../../../README.md?raw'
-import README_ZH_CN from '../../../README.ZH_CN.md?raw'
+import README_ZH_CN from '../../../README.zh-CN.md?raw'
 import css from './HomeView.module.css'
 import { LanguageEnum } from '@liuli-util/i18next-util'
 import { getLanguage } from '../../common/getLanguage'
 import { useLocalStorage } from 'react-use'
-
-const markdownIt = new MarkdownIt()
+import ReactMarkdown from 'react-markdown'
 
 export const HomeView: React.FC = () => {
   const [language] = useLocalStorage<LanguageEnum>('language', getLanguage())
   return (
     <Card>
-      <div
-        className={css.home}
-        dangerouslySetInnerHTML={{
-          __html: markdownIt.render(
-            language === LanguageEnum.En ? README : README_ZH_CN,
-          ),
-        }}
-      />
+      <ReactMarkdown className={css.home}>
+        {language === LanguageEnum.En ? README : README_ZH_CN}
+      </ReactMarkdown>
     </Card>
   )
 }
