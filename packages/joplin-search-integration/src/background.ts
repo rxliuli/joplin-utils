27e68c84c3bc72baf21eb57c9ef25d3ebb0ad061
rxliuli <rxliuli@gmail.com>
2023-01-10 19:51:22 +0800

    feat: 自动在临近的位置打开新标签页

--- packages/joplin-search-integration/src/background.ts
+++ packages/joplin-search-integration/src/background.ts
@@ -1,13 +1,14 @@
 import browser from 'webextension-polyfill'
 
-browser.runtime.onMessage.addListener((message) => {
+browser.runtime.onMessage.addListener(async (message) => {
   switch (message.action) {
     case 'open':
       const p = new URLSearchParams(location.search)
       const { action, ...args } = message
       Object.entries(args).forEach(([k, v]) => (Array.isArray(v) ? v.forEach((i) => p.set(k, i)) : p.set(k, String(v))))
       const url = browser.runtime.getURL(`/options/index.html?${p.toString()}`)
-      browser.tabs.create({ url, active: true })
+      const t = (await browser.tabs.query({ active: true, currentWindow: true }))[0]
+      await browser.tabs.create({ url, active: true, index: (t?.index ?? 0) + 1 })
       break
     default:
       break
