53fc089d8ebe705800c71ba0f525dcebdf6c6161
rxliuli <rxliuli@gmail.com>
2024-08-05 16:41:35 +0800

    fix(joplin-plugin-api): 修复插件的 API 没有生成 dts 的错误

--- packages/joplin-plugin-api/vite.config.ts
+++ packages/joplin-plugin-api/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import { node } from '@liuli-util/vite-plugin-node'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [node({ dts: true, entry: ['src/index.ts', 'src/types.ts'] })],
+  plugins: [
+    node({ dts: true, entry: ['src/index.ts', 'src/types.ts'] }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/**/*.d.ts',
+          dest: '',
+        },
+      ],
+    }),
+  ],
 })
