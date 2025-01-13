1d31f2a08326856d46008e26a9b9ac8ee7625ebe
rxliuli <rxliuli@gmail.com>
2025-01-13 22:00:51 +0800

    chore: archive joplin-batch-web

--- archives/joplin-batch-web/src/common/getLanguage.ts
+++ archives/joplin-batch-web/src/common/getLanguage.ts
@@ -1,7 +1,5 @@
 import { LanguageEnum } from '@liuli-util/i18next-util'
 
 export function getLanguage(): LanguageEnum {
-  return navigator.language.startsWith('zh')
-    ? LanguageEnum.ZhCN
-    : LanguageEnum.En
+  return navigator.language.startsWith('zh') ? LanguageEnum.ZhCN : LanguageEnum.En
 }
